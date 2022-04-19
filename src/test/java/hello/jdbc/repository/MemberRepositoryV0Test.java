e3c9763301e6dce8534a1cea5776271f43f80f78
DahamLeee <dlek1011@gmail.com>
2022-04-19 12:02:01 +0900

    [JDBC - Read] Select Data by Id

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,31 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV_0", 10_000);
+        // save
+        Member member = new Member("memberV_3", 10_000);
         repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        // equals 와 hashcode 는 이번에 정리하자.
+        log.info("findMember={}", findMember);
+        log.info("member == findMember {}", member == findMember);
+        log.info("member equals findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
     }
 }
